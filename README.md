f7aac95977bc7d229f296f8c6868ed4a8582d887
Thomas Lescieux <thomas.lescieux@live.fr>
2024-04-20 20:13:55 +0200

    Creating vue app, datas & structures, conditionnal rendering

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# vue_basics
+# Vue.js tutorial
+Based on this set of videos : 
+https://www.youtube.com/playlist?list=PL4cUxeGkcC9hYYGbV60Vq3IXYNfDk8At1
--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const app = Vue.createApp({
-    // data, functions
-    // template: '<h2>I am the template</h2>'
+    data() {
+        return {
+            showBooks: false,
+            title: 'The Final Empire',
+            author: 'Brandon Sanderson',
+            age: '45'
+
+        }
+    },
+    methods: {
+        changeTitle(title) {
+            this.title = title
+        },
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks;
+        }
+    }
 })
 
 app.mount('#app')
