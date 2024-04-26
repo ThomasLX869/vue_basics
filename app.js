531dbd0ac647ecf287b683c9b54526ee0c219872
Thomas Lescieux <thomas.lescieux@live.fr>
2024-04-26 10:48:02 +0200

    attribute binding

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const app = Vue.createApp({
     data() {
         return {
+            url: 'https://github.com/ThomasLX869',
             showBooks: false,
             books: [
-                { title : 'The Final Empire', author : 'Brandon Sanderson', age: '45'},
-                { title : 'Harry Poter', author : 'JK Rowling', age: '25'},
-                { title : 'Le Horla', author : 'Guy de Maupassant', age: '133'},
+                { title : 'The Final Empire', author : 'Brandon Sanderson', age: '45', img: 'assets/the_final_empire.jpg'},
+                { title : 'Harry Poter', author : 'JK Rowling', age: '25', img: 'assets/harry_potter.jpg'},
+                { title : 'Le Horla', author : 'Guy de Maupassant', age: '133', img: 'assets/horla.jpg'},
             ],
             x: 0,
             y: 0
