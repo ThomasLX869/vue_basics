95ed4ab4edd67ac0967223ded11d16895ce335a9
Thomas Lescieux <thomas.lescieux@live.fr>
2024-05-05 13:02:37 +0200

    slots

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -2,7 +2,14 @@
   <h1>{{ title }}</h1>
   <p>Welcome...</p>
   <div v-if="showModal">
-    <Modal :header="header" :text="text" theme="sale" @close="toggleOpenModal"/>
+    <Modal theme="sale" @close="toggleOpenModal">
+      <template v-slot:links>
+        <a href="#">sign up now</a>
+        <a href="#">more info</a>
+      </template>
+      <h1>Ninja giveaway !</h1>
+      <p>Grab your ninja swag for half price !</p>
+    </Modal>
   </div>
   <button @click.alt="toggleOpenModal"> Open modal (alt) </button>
 </template>
