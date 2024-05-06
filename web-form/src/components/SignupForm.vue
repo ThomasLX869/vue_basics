7a115e81df933db1a4982f53a56c8bfbb76ac10b
Thomas Lescieux <thomas.lescieux@live.fr>
2024-05-06 10:28:30 +0200

    using v-model

--- web-form/src/components/SignupForm.vue
+++ web-form/src/components/SignupForm.vue
@@ -1,13 +1,23 @@
 <template>
   <form>
     <label>Email :</label>
-    <input type="email" required>
+    <input type="email" required v-model="email">
+    
+    <label>Password :</label>
+    <input type="password" required v-model="password">
   </form>
+  <p>Email : {{ this.email }}</p>
+  <p>Password : {{ this.password }}</p>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            email: '',
+            password: ''
+        }
+    }
 }
 </script>
 
