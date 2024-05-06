485b27f27a6602cd5447c8570a8405955af69913
Thomas Lescieux <thomas.lescieux@live.fr>
2024-05-06 10:49:16 +0200

    using v-model on checkboxes

--- web-form/src/components/SignupForm.vue
+++ web-form/src/components/SignupForm.vue
@@ -11,10 +11,16 @@
         <option value="developer">Web Developer</option>
         <option value="designer">Web Designer</option>
     </select>
+
+    <div class="terms">
+        <input type="checkbox" v-model="terms" required>
+        <label> Accept terms and conditions</label>
+    </div>
   </form>
   <p>Email : {{ this.email }}</p>
   <p>Password : {{ this.password }}</p>
   <p>Role : {{ this.role }}</p>
+  <p>Terms accepted : {{ this.terms }}</p>
 </template>
 
 <script>
@@ -23,7 +29,8 @@ export default {
         return {
             email: '',
             password: '',
-            role: 'developer'
+            role: 'developer',
+            terms: false
         }
     }
 }
@@ -55,5 +62,12 @@ export default {
         border: none;
         border-bottom: 1px solid #ddd;
         color: #555;
+    }
+    input[type="checkbox"] {
+        display: inline-block;
+        width: 16px;
+        margin: 0 10px 0 0;
+        position: relative;
+        top: 2px;
     }
 </style>
